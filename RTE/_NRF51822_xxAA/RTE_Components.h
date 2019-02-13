a4ff5f6d30f3b4127f48353584150cc2c75e9a6a
Viacheslav <viacheslav@mcublog.xyz>
2019-02-13 22:28:23 +0300

    added project files;

---
+++ RTE/_NRF51822_xxAA/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'BLE400_ble_uart' 
+ * Target:  'NRF51822_xxAA' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define GPIOTE_ENABLED
+  #define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define S130
+#define SOFTDEVICE_PRESENT
+#define SWI_DISABLE0
+#define UART0_ENABLED
+
+#endif /* RTE_COMPONENTS_H */
